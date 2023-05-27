83f071a404ffe55fbd33aa5aa9cc6d4b211aed80
parker.t.harrelson <parker.t.harrelson@jbhunt.com>
2023-05-26 22:14:09 -0500

    first commit

---
+++ webservices/ws_flexjunction_user_management/src/main/java/com/flexjunction/usermanagement/WsFlexjunctionUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.flexjunction.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsFlexjunctionUserManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsFlexjunctionUserManagementApplication.class, args);
+    }
+
+}
